f5872ac68bbb5365a4bc75a9c21ee6f82f5a2495
Anshul Mishra <anshul8839214019@gmail.com>
2026-06-01 11:11:57 +0530

    backend is prepared

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,31 +3,32 @@ const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
-
 require("./config/redis");
 
 const authRoutes = require("./routes/authRoutes");
 const imageRoutes = require("./routes/imageRoutes");
+const processRoutes = require("./routes/processRoutes");
 const { standardLimiter } = require("./middleware/rateLimiter");
+const errorHandler = require("./middleware/errorHandler"); // 1. Import global handler
 
 const app = express();
 
-// Database Handshake
 connectDB();
 
-// Global Setup
 app.set("trust proxy", 1);
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "2mb" }));
 app.use(standardLimiter);
 
-// Endpoint Assignments
+// Endpoints
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/images", imageRoutes);
+app.use("/api/v1/process", processRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "healthy", project: "Pixel DIP Engine" });
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
